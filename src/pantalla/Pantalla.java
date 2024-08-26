657dd7d3c5f5626bbe4a64aa4faf1f8a64e7608a
Snorlax555 <cami.n.molina@gmail.com>
2024-08-25 22:02:01 -0300

    GUI2

---
+++ src/pantalla/Pantalla.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pantalla;
+
+/**
+ *
+ * @author camin
+ */
+public class Pantalla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventana espotify = new ventana();
+        espotify.setVisible(true);
+    }
+    
+}
