43bc39cfba7ec0c66d464349742b65f68184bead
Snorlax555 <cami.n.molina@gmail.com>
2024-08-27 09:09:01 -0300

    Usuario

---
+++ src/Espotify/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Espotify;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Cliente extends Usuario {
+    
+    //private puntero Seguir;
+    //private puntero Siguen;
+}
