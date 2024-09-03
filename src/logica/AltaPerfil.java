c69b0b09b068411731983b2f675eacc5473aafb8
Snorlax555 <cami.n.molina@gmail.com>
2024-09-03 18:36:59 -0300

    ventana

---
+++ src/logica/AltaPerfil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import Espotify.ventana
+/**
+ *
+ * @author Camilo
+ */
+public class AltaPerfil {
+    Object nombre = Espotify.ventana.TextField1.getText();
+    
+}
