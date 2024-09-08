91350664515c8f94cef0d8b536f6b9ba05eda1a5
Snorlax555 <cami.n.molina@gmail.com>
2024-09-07 22:37:24 -0300

    ventana

--- src/Espotify/Playlist.java
+++ src/Espotify/Playlist.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Espotify;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,21 +13,33 @@ import javax.persistence.OneToMany;
  */
 
 @Entity
-public class Playlist implements Serializable{
+public class Playlist implements Serializable {
     @Id
-    @Column (name="NOMBRE")
+    @Column(name="NOMBRE")
     protected String nombre;
+
     @OneToMany
-    private Tema temas;
-    //imagen;
-    public Playlist(){}
-    public Playlist(String NuevoNombre){
-        nombre = NuevoNombre;
+    private List<Tema> temas;
+
+    public Playlist() {}
+
+    public Playlist(String nuevoNombre) {
+        this.nombre = nuevoNombre;
     }
-    public String getNombre(){
+
+    public String getNombre() {
         return nombre;
     }
-    public void setNombre(String NuevoNombre){
-        this.nombre = NuevoNombre;
+
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    public List<Tema> getTemas() {
+        return temas;
+    }
+
+    public void setTemas(List<Tema> temas) {
+        this.temas = temas;
     }
 }
