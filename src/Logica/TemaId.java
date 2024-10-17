f69885289f984a7dda971dfd5e4dc6539bd7b2e5
Snorlax555 <cami.n.molina@gmail.com>
2024-10-17 18:42:38 -0300

    Layouts # Conflicts: #	src/Espotify/InfiniteVoid.java

---
+++ src/Logica/TemaId.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class TemaId implements Serializable {
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "ALBUM")
+    private String albumNombre;
+
+    // Constructor por defecto
+    public TemaId() {
+    }
+
+    // Constructor con parámetros
+    public TemaId(String nombre, String albumNombre) {
+        this.nombre = nombre;
+        this.albumNombre = albumNombre;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAlbumNombre() {
+        return albumNombre;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAlbumNombre(String albumNombre) {
+        this.albumNombre = albumNombre;
+    }
+
+    // equals y hashCode para asegurar que dos objetos TemaId sean iguales cuando los valores son iguales
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TemaId temaId = (TemaId) o;
+
+        if (!nombre.equals(temaId.nombre)) {
+            return false;
+        }
+        return albumNombre.equals(temaId.albumNombre);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nombre.hashCode();
+        result = 31 * result + albumNombre.hashCode();
+        return result;
+    }
+}
