1ae5ac3a5e1798110125bdc806585636caff96b3
Thiago Cedrés <104656328+Cedrego@users.noreply.github.com>
2024-10-20 20:32:59 -0300

    Merge origin/master
    # Conflicts:
    #	src/Espotify/ventana.java

--- src/Logica/Ctrl.java
+++ src/Logica/Ctrl.java
@@ -62,25 +62,6 @@ public class Ctrl implements ICtrl{
         AltaGenero AG = new AltaGenero(nomG,padre);
     }
     
-    @Override
-    public boolean existePassC(String NOE,String Contra){
-        em = clienteController.getEntityManager();
-        List<String> Cliente = em.createNativeQuery("Select NICK from cliente where (NICK = ?1 or MAIL = ?1) and CONTRASENIA = ?2")
-            .setParameter(1, NOE)
-            .setParameter(2, Contra)
-            .getResultList();
-        return !Cliente.isEmpty();
-    }
-    
-    @Override
-    public boolean existePassA (String NOE, String Contra){
-        em = artistaController.getEntityManager();
-        List<String> Artistas = em.createNativeQuery("Select NICK from artista where (NICK = ?1 or MAIL = ?1) and CONTRASENIA = ?2")
-            .setParameter(1, NOE)
-            .setParameter(2, Contra)
-            .getResultList();
-        return !Artistas.isEmpty();
-    }
     
     @Override
     public void crearArtista(String nick, String nom, String ape, String mail, String pass, int dia, String mes, int anio, String bio, String web){
@@ -403,7 +384,26 @@ public class Ctrl implements ICtrl{
             return false;
         }
     }
-    
+    @Override
+    public boolean existePassC(String NOE, String Contra) {
+        em = clienteController.getEntityManager();
+        List<String> Cliente = em.createNativeQuery("Select NICK from cliente where (NICK = ?1 or MAIL = ?1) and CONTRASENIA = ?2")
+                .setParameter(1, NOE)
+                .setParameter(2, Contra)
+                .getResultList();
+        return !Cliente.isEmpty();
+    }
+
+    @Override
+    public boolean existePassA(String NOE, String Contra) {
+        em = artistaController.getEntityManager();
+        List<String> Artistas = em.createNativeQuery("Select NICK from artista where (NICK = ?1 or MAIL = ?1) and CONTRASENIA = ?2")
+                .setParameter(1, NOE)
+                .setParameter(2, Contra)
+                .getResultList();
+        return !Artistas.isEmpty();
+    }
+
     @Override
     public boolean esPrivado(String nombrePart, String nombreCliente){
         Particular part = particularJpaController.findParticular(nombrePart, nombreCliente);
